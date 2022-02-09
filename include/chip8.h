7f2158ffb3e7bbfc25f590b9803e0af78faa41d7
shaurya <sharmashaurya2907@gmail.com>
2022-02-09 19:51:43 +0530

    test

---
+++ include/chip8.h
@@ -0,0 +1,17 @@
+#ifndef CHIP8_H
+#define CHIP8_H
+
+#include "config.h"
+#include "chip8memory.h"
+#include "chip8registers.h"
+
+struct chip8{
+
+    struct chip8_memory memory;
+    struct chip8_registers registers;
+
+
+
+};
+
+#endif
